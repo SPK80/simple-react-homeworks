086ab96bd3827641309905011e888760a99dfce9
SPK80 <psukonkin@gmail.com>
2022-05-12 06:17:03 +0300

    hw2

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,21 +1,38 @@
 import React from 'react'
+import {AffairPriorityType, AffairType} from "./HW2";
+import s from "./Affairs.module.css"
 
 type AffairPropsType = {
-    // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+	// key не нужно типизировать
+	affair: AffairType
+	deleteAffairCallback: (_id: number) => void
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
-
-    return (
-        <div>
-            // show some text
-
-            <button onClick={deleteCallback}>X</button>
-        </div>
-    )
+	const deleteCallback = () => {
+		props.deleteAffairCallback(props.affair._id)
+	}
+	
+	const getPriorityStyle = (priority: AffairPriorityType) => {
+		switch (priority) {
+			case "low":
+				return s.lowPriority
+			case "middle":
+				return s.middlePriority
+			case "high":
+				return s.highPriority
+		}
+	}
+	
+	return (
+		<div className={s.affair}>
+			<div className={s.name}>{props.affair.name}</div>
+			<div>
+				<div className={getPriorityStyle(props.affair.priority)}>{props.affair.priority}</div>
+				<button onClick={deleteCallback}>❌</button>
+			</div>
+		</div>
+	)
 }
 
 export default Affair
