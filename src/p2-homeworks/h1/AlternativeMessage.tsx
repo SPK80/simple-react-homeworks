4f6ff859461446e483ccdd659f3692f837f1c0d4
SPK80 <psukonkin@gmail.com>
2022-05-12 06:16:32 +0300

    hw1

--- src/p2-homeworks/h1/AlternativeMessage.tsx
+++ src/p2-homeworks/h1/AlternativeMessage.tsx
@@ -2,39 +2,41 @@ import React from 'react'
 import s from "./AlternativeMessage.module.css";
 
 type AlternativeMessagePropsType = {
-  avatar: string
-  name: string
-  message: string
-  time: string
+	avatar: string
+	name: string
+	message: string
+	time: string
 }
 
-export const AlternativeMessage = (props: AlternativeMessagePropsType) => {
-  return (
-    <div className={s.message}>
-      
-      <img className={s.avatar}
-           src={props.avatar}
-           alt={'avatar'}
-      />
-      
-      <div className={s.corner}>
-        <div className={s.circle}></div>
-        <div className={s.square}></div>
-      </div>
-      
-      <div className={s.messageBox}>
-        <div>
-          <div className={s.name}>
-            {props.name}
-          </div>
-          <div className={s.messageText}>
-            {props.message}
-          </div>
-        </div>
-        <div className={s.time}>
-          {props.time}
-        </div>
-      </div>
-    </div>
-  )
+const AlternativeMessage = (props: AlternativeMessagePropsType) => {
+	return (
+		<div className={s.message}>
+			
+			<img className={s.avatar}
+					 src={props.avatar}
+					 alt={'avatar'}
+			/>
+			
+			<div className={s.corner}>
+				<div className={s.circle}></div>
+				<div className={s.square}></div>
+			</div>
+			
+			<div className={s.messageBox}>
+				<div>
+					<div className={s.name}>
+						{props.name}
+					</div>
+					<div className={s.messageText}>
+						{props.message}
+					</div>
+				</div>
+				<div className={s.time}>
+					{props.time}
+				</div>
+			</div>
+		</div>
+	)
 }
+
+export default AlternativeMessage;
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -2,39 +2,41 @@ import React from 'react'
 import s from './Message.module.css'
 
 type MessagePropsType = {
-  avatar: string
-  name: string
-  message: string
-  time: string
+	avatar: string
+	name: string
+	message: string
+	time: string
 }
 
-export const Message = (props: MessagePropsType) => {
-  return (
-    <div className={s.message}>
-      
-      <img className={s.avatar}
-           src={props.avatar}
-           alt={'avatar'}
-      />
-      
-      <div className={s.corner}>
-        <div className={s.circle}></div>
-        <div className={s.square}></div>
-      </div>
-      
-      <div className={s.messageBox}>
-        <div>
-          <div className={s.name}>
-            {props.name}
-          </div>
-          <div className={s.messageText}>
-            {props.message}
-          </div>
-        </div>
-        <div className={s.time}>
-          {props.time}
-        </div>
-      </div>
-    </div>
-  )
+const Message = (props: MessagePropsType) => {
+	return (
+		<div className={s.message}>
+			
+			<img className={s.avatar}
+					 src={props.avatar}
+					 alt={'avatar'}
+			/>
+			
+			<div className={s.corner}>
+				<div className={s.circle}></div>
+				<div className={s.square}></div>
+			</div>
+			
+			<div className={s.messageBox}>
+				<div>
+					<div className={s.name}>
+						{props.name}
+					</div>
+					<div className={s.messageText}>
+						{props.message}
+					</div>
+				</div>
+				<div className={s.time}>
+					{props.time}
+				</div>
+			</div>
+		</div>
+	)
 }
+
+export default Message;
