acddd9b6dd0dccee3f027d4d5da457f3cc6870f3
SPK80 <psukonkin@gmail.com>
2022-05-20 15:07:25 +0300

    refactor: in Greeting changes: input to SuperInputText and button to SuperButton

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,5 +1,7 @@
 import React, {ChangeEvent, KeyboardEvent} from 'react'
 import s from './Greeting.module.css'
+import SuperInputText from "../h4/common/c1-SuperInputText/SuperInputText";
+import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
 
 type GreetingPropsType = {
     name: string
@@ -13,30 +15,31 @@ type GreetingPropsType = {
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = error ? s.inputError : ''
+    // const inputClass = error ? s.inputError : ''
 
-    const onKeyPressHandler = (e:KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter') {
-            addUser()
-        }
-    };
+    // const onKeyPressHandler = (e:KeyboardEvent<HTMLInputElement>) => {
+    //     if (e.key === 'Enter') {
+    //         addUser()
+    //     }
+    // };
 
     return (
         <div className={s.greeting}>
-            <div>
-                <input
-                    className={inputClass}
+                <SuperInputText
+                    // className={inputClass}
                     value={name}
                     onChange={setNameCallback}
-                    onKeyPress={onKeyPressHandler}
+                    // onKeyPress={onKeyPressHandler}
+                    onEnter={addUser}
+                    spanClassName={s.errorMessage}
+                    error={error?'Please enter you name':''}
                 />
-                <button
+                <SuperButton
                     onClick={addUser}
                     disabled={name === ''}
                 > add
-                </button>
-            </div>
-            {error && <div className={s.errorMessage}>Please enter you name</div>}
+                </SuperButton>
+            {/*{error && <div className={s.errorMessage}>Please enter you name</div>}*/}
             <span className={s.totalUsers}>users: {totalUsers}</span>
         </div>
     )
