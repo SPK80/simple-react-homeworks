a53d1dd1191988d26badee611fc95bb298cbce30
SPK80 <psukonkin@gmail.com>
2022-06-13 15:41:42 +0300

    feat: implement new style for SuperSelect

--- src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
+++ src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -1,4 +1,5 @@
 import React, {SelectHTMLAttributes, DetailedHTMLProps, ChangeEvent} from 'react'
+import s from "./SuperSelect.module.css"
 
 type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
 
@@ -24,7 +25,11 @@ const SuperSelect: React.FC<SuperSelectPropsType> = (
     }
     
     return (
-        <select onChange={onChangeCallback} {...restProps}>
+        <select
+            className={s.superSelect}
+            onChange={onChangeCallback}
+            {...restProps}
+        >
             {mappedOptions}
         </select>
     )
