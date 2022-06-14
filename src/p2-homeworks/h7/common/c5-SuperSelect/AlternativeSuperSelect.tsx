9a7e12b8809396ebe1f468e457afbad5060c51af
SPK80 <psukonkin@gmail.com>
2022-06-14 16:22:55 +0300

    feat: implement AlternativeSuperSelect SuperSelect

--- src/p2-homeworks/h7/common/c5-SuperSelect/AlternativeSuperSelect.tsx
+++ src/p2-homeworks/h7/common/c5-SuperSelect/AlternativeSuperSelect.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, {DetailedHTMLProps, SelectHTMLAttributes, useState} from 'react'
+import s from './AlternativeSuperSelect.module.css'
 
-function AlternativeSuperSelect() {
-    return (
-        <input/>
-    )
+type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
+
+type PropsType = DefaultSelectPropsType & {
+    options?: string[]
+    onChangeOption?: (option: string) => void
 }
+const AlternativeSuperSelect: React.FC<PropsType> =
+    ({
+         options,
+         value,
+         onChangeOption
+     }) => {
+        
+        const [listShow, setListShow] = useState(false)
+        
+        const mappedOptions: JSX.Element[] =
+            options ? options.map((opt, i) => {
+                        const onClickOptionHandler = () =>
+                            onChangeOption && onChangeOption(opt)
+                        
+                        return (
+                            <span
+                                key={i}
+                                onClick={onClickOptionHandler}
+                            >
+                                {opt}
+                            </span>
+                        )
+                    }
+                )
+                : []
+        
+        const onClickValueHandler = () => setListShow(!listShow)
+        const onBlurValueHandler = () => setListShow(false)
+        
+        return (
+            <span
+                className={s.value}
+                onClick={onClickValueHandler}
+                onBlur={onBlurValueHandler}
+            >
+                {value}
+                {listShow && <span className={s.options}>
+                    {mappedOptions}
+                </span>}
+            </span>
+        )
+    }
 
 export default AlternativeSuperSelect
