2505ce61b3bf02c9931fb064609bd91e15b97ebd
SPK80 <psukonkin@gmail.com>
2022-06-22 16:17:38 +0300

    feat: HW8: implement homeWorkReducer tests and homeWorkReducer

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,27 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type SortActionType = {
+    type: 'sort'
+    payload: 'up' | 'down'
+}
+
+type CheckActionType = {
+    type: 'check'
+    payload: number
+}
+
+type AllActionTypes = SortActionType | CheckActionType
+
+export const homeWorkReducer = (state: Array<UserType>, action: AllActionTypes): Array<UserType> => {
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            return (action.payload == "up") ? [...state].sort((a, b) => (a.name > b.name) ? 1 : -1)
+                : [...state].sort((a, b) => (a.name < b.name) ? 1 : -1)
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(user => user.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
