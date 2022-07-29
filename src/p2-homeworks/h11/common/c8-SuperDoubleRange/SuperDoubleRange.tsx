1be4f91bf332914351fe957d240c0a1267775044
SPK80 <psukonkin@gmail.com>
2022-07-29 06:51:07 +0300

    feat: HW11

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -13,35 +13,45 @@ const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
         // min, max, step, disable, ...
     }
 ) => {
-
+    
     const onChangeLowerCallback = (e: ChangeEvent<HTMLInputElement>) => {
         value && onChangeRange && onChangeRange([+e.currentTarget.value, value[1]])
     }
-
+    
     const onChangeUpperCallback = (e: ChangeEvent<HTMLInputElement>) => {
         value && onChangeRange && onChangeRange([value[0], +e.currentTarget.value])
     }
-
+    
     return (
-        <span className={s.range}>
-            <input
-                type="range"
-                min={0}
-                max={50}
-                value={10}
-                id="lower"
-                onChange={onChangeLowerCallback}
-            />
-            <input
-                type={"range"}
-                min={0}
-                max={50}
-                value={10}
-                id="upper"
-                onChange={onChangeUpperCallback}
-            />
-        </span>
+        <div className={s.range}>
+            <div
+                className={s.slider + ' ' + s.lower}
+            
+            ></div>
+            <div
+                className={s.slider + ' ' + s.upper}
+            
+            ></div>
+        </div>
     )
 }
 
 export default SuperDoubleRange
+
+
+// <input
+// type="range"
+// min={0}
+// max={50}
+// value={10}
+// id="lower"
+// onChange={onChangeLowerCallback}
+// />
+// <input
+//     type={"range"}
+//     min={0}
+//     max={50}
+//     value={10}
+//     id="upper"
+//     onChange={onChangeUpperCallback}
+// />
