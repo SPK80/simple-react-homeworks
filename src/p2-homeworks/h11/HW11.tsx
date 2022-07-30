0d951469f38444d16118a1d7b91eec1c4aeb22ea
SPK80 <psukonkin@gmail.com>
2022-07-30 19:08:31 +0300

    feat: HW11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,42 +1,39 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
-import {Button, Slider} from "antd";
+import {Slider} from "antd";
+import 'antd/dist/antd.css';
 
 function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
-
+    
     return (
         <div>
-            <Button type="primary">Primary Button</Button>
-            <Slider range defaultValue={[20, 50]} disabled={false}/>
-
             <hr/>
             homeworks 11
-
-            {/*should work (должно работать)*/}
             <div>
                 <span>{value1}</span>
                 <SuperRange
-                    // сделать так чтоб value1 изменялось
+                    value={value1}
                     onChangeRange={setValue1}
                 />
+            
             </div>
-
+            
             <div>
-
-                {/*<span>{value1}</span>*/}
-                {/*<SuperDoubleRange*/}
-                {/*    value={[value1, value2]}*/}
-                {/*    onChangeRange={([value1, value2]) => {*/}
-                {/*        setValue1(value1)*/}
-                {/*        setValue2(value2)*/}
-                {/*    }}*/}
-                {/*    // сделать так чтоб value1 и value2 изменялось*/}
-                {/*/>*/}
-                {/*<span>{value2}</span>*/}
+                <Slider
+                    dots={true}
+                    style={{width: 150}}
+                    range
+                    value={[value1, value2]}
+                    onChange={([value1, value2]) => {
+                        setValue1(value1);
+                        setValue2(value2);
+                    }}
+                />
+                <span>{value2}</span>
             </div>
-
+            
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeSuperRange/>*/}
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,9 +1,8 @@
-import React, {ChangeEvent, MouseEvent} from 'react'
-import s from "./SuperDoubleRange.module.css";
+import React from 'react'
 
 type SuperDoubleRangePropsType = {
-    onChangeRange: (value: [number, number]) => void
-    value: [number, number]
+    onChangeRange?: (value: [number, number]) => void
+    value?: [number, number]
     // min, max, step, disable, ...
 }
 
@@ -13,81 +12,13 @@ const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
         // min, max, step, disable, ...
     }
 ) => {
-    console.log('value:', value)
-    // const onChangeLowerCallback = (e: ChangeEvent<HTMLInputElement>) => {
-    //     onChangeRange([+e.currentTarget.value, value[1]])
-    // }
-
-    // const onChangeUpperCallback = (e: ChangeEvent<HTMLInputElement>) => {
-    //     onChangeRange([value[0], +e.currentTarget.value])
-    // }
-
-    // const onMouseMoveRange = (e: MouseEvent<HTMLDivElement>) => {
-    //     e.preventDefault()
-    //     console.log({x: e.clientX, y: e.clientY})
-    // }
-    // const onClickRange = (e: MouseEvent<HTMLDivElement>) => {
-    //     e.preventDefault()
-    //     // onChangeRange([e.clientX - e.currentTarget.offsetLeft, 0])
-    //     console.log(e.clientX - e.currentTarget.offsetLeft)
-    // }
-
-    const [value1, value2] = value;
-
-    // function onClickLower(e: MouseEvent<HTMLDivElement>) {
-    //     e.preventDefault()
-    //     console.log(e.clientX - e.currentTarget.offsetLeft)
-    // }
-    //
-    // function onClickUpper(e: MouseEvent<HTMLDivElement>) {
-    //     e.preventDefault()
-    //     console.log(e.clientX - e.currentTarget.offsetLeft)
-    // }
-
-    function onMouseMove(e: MouseEvent<HTMLDivElement>) {
-        e.preventDefault()
-        // console.log(e.buttons)
-        if (e.buttons === 0) return
-
-        console.log(e.clientX)
-        // onChangeRange([value[0] + e.movementX, value[1]])
-    }
-
+    // сделать самому, можно подключать библиотеки
+    
     return (
-        <div className={s.range}
-            // onMouseMove={onMouseMoveRange}
-        >
-            <div
-                className={s.slider + ' ' + s.lower}
-                style={{left: value1}}
-                // onClick={onClickLower}
-                onMouseMove={onMouseMove}
-            ></div>
-            <div
-                className={s.slider + ' ' + s.upper}
-                style={{left: value2}}
-                // onClick={onClickUpper}
-            ></div>
-        </div>
+        <>
+            DoubleRange
+        </>
     )
 }
 
 export default SuperDoubleRange
-
-
-// <input
-// type="range"
-// min={0}
-// max={50}
-// value={10}
-// id="lower"
-// onChange={onChangeLowerCallback}
-// />
-// <input
-//     type={"range"}
-//     min={0}
-//     max={50}
-//     value={10}
-//     id="upper"
-//     onChange={onChangeUpperCallback}
-// />
